cbe18b3416d2a046bfb15e0f589a45690cb24e58
Allkoman <18646085515@163.com>
2018-04-07 16:31:47 +0800

    register done

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -1,7 +1,48 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: zhangchao
- * Date: 2018/3/29
- * Time: 15:35
+ * @name IndexController
+ * @author zhangchao
+ * @desc 默认控制器
+ * @see http://www.php.net/manual/en/class.yaf-controller-abstract.php
  */
+class UserController extends Yaf_Controller_Abstract
+{
+
+    public function indexAction()
+    {
+        return $this->loginAction();
+    }
+
+    public function loginAction()
+    {
+
+    }
+
+    public function registerAction()
+    {
+        $uName = $this->getRequest()->getPost("uName", false);
+        $pwd = $this->getRequest()->getPost("pwd", false);
+        if (!$uName || !$pwd) {
+            echo json_encode(array("errNo" => -1002, "errMsg" => "用户名和密码必须传递"));
+            return FALSE;
+        }
+
+        $model = new UserModel();
+        if ($model->register(trim($uName), trim($pwd))) {
+            echo json_encode(array(
+                "errNo" => 0,
+                "errMsg" => "",
+                "data" => array("name" => $uName)
+            ));
+        } else {
+            echo json_encode(array(
+                "errNo" => $model->errNo,
+                "errMsg" => $model->errMsg
+            ));
+        }
+        return TRUE;
+    }
+
+
+}
--- application/models/User.php
+++ application/models/User.php
@@ -1,7 +1,54 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: zhangchao
- * Date: 2018/3/29
- * Time: 15:35
+ * @name UserModel
+ * @desc 登录注册
+ * @author zhangchao
  */
+class UserModel
+{
+    public $errNo = 0;
+    public $errMsg = "";
+    private $_db = null;
+
+    public function __construct()
+    {
+        $this->_db = new PDO("mysql:host=localhost;dbname=imooc;", "root", "themis");
+    }
+
+    public function register($uName, $pwd)
+    {
+        $query = $this->_db->prepare("select count(*) as c from `user` where `name` = ? ");
+        $query->execute(array($uName));
+        $count = $query->fetchAll();
+
+        if ($count[0]['c'] != 0) {
+            $this->errNo = -1005;
+            $this->errMsg = "用户名已存在";
+            return false;
+        }
+
+        if (strlen($pwd) < 8) {
+            $this->errNo = -1006;
+            $this->errMsg = "密码太短，请至少设置8位的密码";
+            return false;
+        } else {
+            $password = $this->_password_generate($pwd);
+        }
+
+        $query = $this->_db->prepare("insert into `user` (`id`, `name`, `pwd`, `reg_time`) values (null, ?, ?, ?)");
+        $ret = $query->execute(array($uName, $pwd, date("Y-m-d H:i:s")));
+        if (!$ret) {
+            $this->errNo = -1006;
+            $this->errMsg = "注册失败，写入数据失败";
+            return false;
+        }
+        return true;
+    }
+
+    private function _password_generate($password)
+    {
+        $pwd = md5("salt-xxxxxxxxxx-" . $password);
+        return $pwd;
+    }
+}
